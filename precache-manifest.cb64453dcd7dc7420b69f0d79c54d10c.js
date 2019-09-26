c3c2545cb66b2a81c2c231ed8a89556afd912eb6
red42 <mperepelitsa421@gmail.com>
2019-09-26 17:14:25 +0300

    Updates

---
+++ precache-manifest.cb64453dcd7dc7420b69f0d79c54d10c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20377347e73c712f911624f2f4762bf3",
+    "url": "/react-blog-app/index.html"
+  },
+  {
+    "revision": "f2a01709af8396af5d6f",
+    "url": "/react-blog-app/static/css/main.a7160021.chunk.css"
+  },
+  {
+    "revision": "92901cb951e680858e04",
+    "url": "/react-blog-app/static/js/2.723b96dd.chunk.js"
+  },
+  {
+    "revision": "f2a01709af8396af5d6f",
+    "url": "/react-blog-app/static/js/main.d20ebd9a.chunk.js"
+  },
+  {
+    "revision": "d4c2c8ec2a2b0e9052e5",
+    "url": "/react-blog-app/static/js/runtime~main.fc2e26ff.js"
+  }
+]);
